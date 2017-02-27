8634a6a632f2bf45d50f0a43b243970d0a45a86c
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-27 10:03:03 -0700

    Add navbar Add current_user method Add user account creation tests Update controllers to show appropriate flash messages to match spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,15 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "You have successfully created an account"
       redirect_to :root
     else
+      flash[:alert] = "There was a problem. #{user.errors.full_messages.join(' ')}. Please try again."
       redirect_to :signup
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
