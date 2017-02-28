d3dac26c90a1e026f949e516ad1752aeee734b76
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-27 21:38:09 -0700

    update tests

--- spec/features/links/user_can_submit_links_spec.rb
+++ spec/features/links/user_can_submit_links_spec.rb
@@ -1,18 +1,6 @@
 require "rails_helper"
 
 describe "user submits link", :js => :true do
-  xscenario "an authorized user can't submit an invalid link" do
-    user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-    visit root_path
-
-    fill_in "URL:", with: "google"
-    fill_in "Title:", with: "google title"
-    click_on "Add Link"
-
-    expect(page).to have_content("There was a problem.")
-  end
   scenario "user only sees their own links" do
     user = create(:user)
     user_2 = create(:user, email: "new@email.com")
